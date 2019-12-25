8dd516404783aae5ff0ca0b27b6624aedd74e2ff
Jackson <jlango@fastmail.com>
2019-12-24 21:00:51 -0800

    green cube

--- bundle.js
+++ bundle.js
@@ -51256,9 +51256,29 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var THREE = __importStar(require("three"));
 var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+function render() {
+    renderer.render(scene, camera);
+}
+function update() {
+    requestAnimationFrame(update);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    render();
+}
 function main() {
     console.log("hey");
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    scene.add(cube);
+    camera.position.z = 5;
+    // Kick off update loop.
+    update();
 }
 main();
 
-},{"three":3}]},{},[4,1,2]);
+},{"three":3}]},{},[4,2,1]);
