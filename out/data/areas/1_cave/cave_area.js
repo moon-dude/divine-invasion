8bd37eef5439ce43f5fe6698db039823c687e575
moon-dude <>
2020-01-27 12:49:33 -0800

    Remove out files

--- out/data/areas/1_cave/cave_area.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var area_data_1 = require("../area_data");
-var cave_b1_1 = require("./cave_b1");
-var cave_b2_1 = require("./cave_b2");
-var cave_b3_1 = require("./cave_b3");
-exports.cave_data = new area_data_1.AreaData([cave_b3_1.level3_data, cave_b2_1.level2_data, cave_b1_1.level1_data]);
